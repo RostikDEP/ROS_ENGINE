2da365d92d114f3f6b89579f01a4f7b016224eb0
RostikDEP <stadnikov2000@gmail.com>
2023-03-07 17:51:40 +0200

    NEw Build

---
+++ build/CMakeFiles/ROS_ENGINE.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ROS_ENGINE.dir/src/main.cpp.o"
+  "CMakeFiles/ROS_ENGINE.dir/src/main.cpp.o.d"
+  "ROS_ENGINE"
+  "ROS_ENGINE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ROS_ENGINE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
